6d2b4b85dbcc9a1ba9535fda9a8efbe976473b08
cw <fromnowwon@gmail.com>
2023-09-02 20:37:46 +0900

    03_탐색&시뮬레이션_08_곶감(모래시계)

--- 03_탐색&시뮬레이션/_08_곶감(모래시계).py
+++ 03_탐색&시뮬레이션/_08_곶감(모래시계).py
@@ -2,3 +2,9 @@ import sys
 sys.stdin = open("input.txt", "r")
 n = int(input())
 a = [list(map(int, input().split())) for _ in range(n)]
+m = int(input())
+
+for i in range(m):
+    h, t, k = map(int, input().split())
+    if t == 0:
+        for _ in range(k):
