0c7907a1c0f2b554885821cea918c0b5fa0be47c
cw <fromnowwon@gmail.com>
2023-09-03 21:10:59 +0900

    03_탐색&시뮬레이션_08_곶감(모래시계)

--- 03_탐색&시뮬레이션/_08_곶감(모래시계).py
+++ 03_탐색&시뮬레이션/_08_곶감(모래시계).py
@@ -6,5 +6,24 @@ m = int(input())
 
 for i in range(m):
     h, t, k = map(int, input().split())
+    # 회전
     if t == 0:
         for _ in range(k):
+            a[h-1].append(a[h-1].pop(0))
+    else:
+        for _ in range(k):
+            a[h-1].insert(0, a[h-1].pop(0))
+res = 0
+s = 0
+e = n-1
+for i in range(n):
+    for j in range(s, e+1):
+        res += a[i][j]
+    if i < n//2:
+        s += 1
+        e -= 1
+    else:
+        s -= 1
+        e += 1
+
+print(res)
