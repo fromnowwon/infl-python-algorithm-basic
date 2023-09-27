356531534b5ad817f56163ab58b2bccf1055b57f
cw <fromnowwon@gmail.com>
2023-09-27 20:32:18 +0900

    05_자료구조(스택, 큐, 해쉬, 힙)_06_응급실

--- 05_자료구조(스택, 큐, 해쉬, 힙)/_06_응급실.py
+++ 05_자료구조(스택, 큐, 해쉬, 힙)/_06_응급실.py
@@ -1,4 +1,5 @@
 import sys
 sys.stdin = open("input.txt", "r")
 n, m = map(int, input().split())
-Q = list(map(int, input().split()))
+Q = [(pos, val) for pos, val in enumerate(list(map(int, input().split())))]
+
