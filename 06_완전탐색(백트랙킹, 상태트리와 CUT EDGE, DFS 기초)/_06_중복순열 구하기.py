719786dfdf8de358bbf2c446cf65ff6970d977b0
cw <fromnowwon@gmail.com>
2023-10-18 20:42:24 +0900

    06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)_06_중복순열 구하기

--- 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_06_중복순열 구하기.py
+++ 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_06_중복순열 구하기.py
@@ -3,6 +3,12 @@ sys.stdin = open("input.txt", "rt")
 input = sys.stdin.readline
 
 def DFS(L):
+    if L == m:
+
+    else:
+        for i in range(1, n + 1):
+            res[L] = i
+            DFS(L + 1)
 
 
 if __name__ == "__main__":
