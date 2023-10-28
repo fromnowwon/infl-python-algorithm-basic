54c301a3893da0a841b69e1224b18972280265c9
cw <fromnowwon@gmail.com>
2023-10-28 14:18:10 +0900

    06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)_09_수열 추측하기

--- 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_09_수열 추측하기.py
+++ 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_09_수열 추측하기.py
@@ -1,6 +1,17 @@
 import sys
-sys.stdin = open("input.txt", "r")
-#def DFS(L, sum):
+sys.stdin = open("input.txt", "rt")
+def DFS(L, sum):
+    if L == n and sum == f:
+        for x in p:
+            print(x, end=' ')
+        sys.exit(0)
+    else:
+        for i in range(1, n + 1):
+            if ch[i] == 0:
+                ch[i] = 1
+                p[L] = i
+                DFS(L + 1, sum + (p[L] * b[L]))
+                ch[i] = 0
 
 if __name__ == "__main__":
     n, f = map(int, input().split())
@@ -9,7 +20,6 @@ if __name__ == "__main__":
     ch = [0] * (n + 1)
 
     for i in range(1, n):
-        b[i] = b[i - 1] * (n - 1) // i
+        b[i] = b[i - 1] * (n - i) // i
 
-    for x in b:
-        print(x, end=' ')
+    DFS(0, 0)
