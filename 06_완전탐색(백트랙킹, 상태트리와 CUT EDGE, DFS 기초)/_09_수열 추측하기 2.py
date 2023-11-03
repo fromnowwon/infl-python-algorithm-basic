7a47fe864acde4556c031d632df40bdb17386a90
cw <fromnowwon@gmail.com>
2023-11-03 21:28:15 +0900

    06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)_09_수열 추측하기 2

--- 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_09_수열 추측하기 2.py
+++ 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_09_수열 추측하기 2.py
@@ -3,7 +3,13 @@ import itertools as it
 sys.stdin = open("input.txt", "rt")
 n, f = map(int, input().split())
 b = [1] * n
+cnt = 0
 
 for i in range(1, n):
     b[i] = b[i - 1] * (n - i) / i
 a = list(range(1, n + 1))
+
+for tmp in it.permutations(a, 3):
+    print(tmp)
+    cnt += 1
+print(cnt)
