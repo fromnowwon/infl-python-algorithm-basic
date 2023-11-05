c49a8aec1ae5648bb6efa78a06497894ee676cc9
cw <fromnowwon@gmail.com>
2023-11-05 16:00:26 +0900

    06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)_09_수열 추측하기 2

--- 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_09_수열 추측하기 2.py
+++ 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_09_수열 추측하기 2.py
@@ -9,7 +9,11 @@ for i in range(1, n):
     b[i] = b[i - 1] * (n - i) / i
 a = list(range(1, n + 1))
 
-for tmp in it.permutations(a, 3):
+for tmp in it.permutations(a):
     sum = 0
     for L, x in enumerate(tmp):
         sum += (x * b[L])
+    if sum == f:
+        for x in tmp:
+            print(x, end=' ')
+        break
