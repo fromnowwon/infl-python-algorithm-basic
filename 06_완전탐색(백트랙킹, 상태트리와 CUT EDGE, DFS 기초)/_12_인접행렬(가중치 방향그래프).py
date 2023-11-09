8131c507ceb037ba975010c8810d0a31e4e0f91d
cw <fromnowwon@gmail.com>
2023-11-09 20:12:35 +0900

    06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)_12_인접행렬(가중치 방향그래프)

--- 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_12_인접행렬(가중치 방향그래프).py
+++ 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_12_인접행렬(가중치 방향그래프).py
@@ -8,3 +8,8 @@ for i in range(m):
     a, b = map(int, input().split())
     g[a][b] = 1
     g[b][a] = 1
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print(g[i][j], end=' ')
+    print()
