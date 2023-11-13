7209d492807895fed17ead3ff6348e5256bbe7ca
cw <fromnowwon@gmail.com>
2023-11-13 20:59:00 +0900

    06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)_13_경로 탐색(그래프 DFS)

--- 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_13_경로 탐색(그래프 DFS).py
+++ 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_13_경로 탐색(그래프 DFS).py
@@ -1,6 +1,16 @@
 import sys
 sys.stdin = open("input.txt", "rt")
 
+def DFS(v):
+    global cnt
+    if v == n:
+        cnt += 1
+    else:
+        for i in range(1, n+1):
+            if g[v][i] == 1 and ch[i] == 0:
+                ch[i] = 1
+                DFS(i)
+                ch[i] = 0
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
@@ -12,3 +22,5 @@ if __name__ == "__main__":
         g[a][b] = 1
     cnt = 0
     ch[1] = 1
+    DFS(1)
+    print(cnt)
