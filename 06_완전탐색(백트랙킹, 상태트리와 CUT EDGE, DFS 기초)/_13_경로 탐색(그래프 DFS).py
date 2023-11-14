5cac8f06ce742e28452260ebbeee8cea3e5c4a85
cw <fromnowwon@gmail.com>
2023-11-14 21:08:25 +0900

    06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)_13_경로 탐색(그래프 DFS)

--- 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_13_경로 탐색(그래프 DFS).py
+++ 06_완전탐색(백트랙킹, 상태트리와 CUT EDGE, DFS 기초)/_13_경로 탐색(그래프 DFS).py
@@ -5,11 +5,16 @@ def DFS(v):
     global cnt
     if v == n:
         cnt += 1
+        for x in path:
+            print(x, end=' ')
+        print()
     else:
         for i in range(1, n+1):
             if g[v][i] == 1 and ch[i] == 0:
                 ch[i] = 1
+                path.append(i)
                 DFS(i)
+                path.pop()
                 ch[i] = 0
 
 if __name__ == "__main__":
@@ -21,6 +26,8 @@ if __name__ == "__main__":
         a, b = map(int, input().split())
         g[a][b] = 1
     cnt = 0
+    path = []
+    path.append(1)
     ch[1] = 1
     DFS(1)
     print(cnt)
