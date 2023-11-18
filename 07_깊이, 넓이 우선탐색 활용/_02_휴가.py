9decdb4d6adee288c33cf49b2bde0031332f078f
cw <fromnowwon@gmail.com>
2023-11-18 20:02:08 +0900

    07_깊이, 넓이 우선탐색 활용_02_휴가

---
+++ 07_깊이, 넓이 우선탐색 활용/_02_휴가.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open("input.txt", "rt")
+
+if __name__ == "__main__":
+    n = int(input())
+    T = list()
+    P = list()
+
+    for i in range(n):
+        a, b = map(int, input().split())
+        T.append(a)
+        P.append(b)
+    res = -2147000000
+    T.insert(0, 0)
+    P.insert(0, 0)
