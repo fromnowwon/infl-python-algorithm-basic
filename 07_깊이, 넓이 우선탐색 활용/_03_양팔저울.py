5b721f20430826644b8a71946932759a8b0e2516
cw <fromnowwon@gmail.com>
2023-11-25 09:45:22 +0900

    07_깊이, 넓이 우선탐색 활용_03_양팔저울

--- 07_깊이, 넓이 우선탐색 활용/_03_양팔저울.py
+++ 07_깊이, 넓이 우선탐색 활용/_03_양팔저울.py
@@ -4,7 +4,8 @@ sys.stdin = open("input.txt", "rt")
 def DFS(L, sum):
     global res
     if L == n:
-
+        if 0 < sum <= s:
+            res.add(sum)
     else:
         DFS(L+1, sum+G[L])
         DFS(L+1, sum-G[L])
