2f83d456be58497e311ad376c088c1dd7fbbbba4
cw <fromnowwon@gmail.com>
2023-11-28 21:08:22 +0900

    07_깊이, 넓이 우선탐색 활용_04_동전 바꿔주기

--- 07_깊이, 넓이 우선탐색 활용/_04_동전 바꿔주기.py
+++ 07_깊이, 넓이 우선탐색 활용/_04_동전 바꿔주기.py
@@ -2,7 +2,12 @@ import sys
 sys.stdin = open("input.txt", "rt")
 
 if __name__ == "__main__":
-    n = int(input())
-    G = list(map(int, input().split()))
-    s = sum(G)
-    res = set()
+    T = int(input())
+    k = int(input())
+    cv = list()
+    cn = list()
+
+    for i in range(k):
+        a, b = map(int, input().split())
+        cv.append()
+        cn.append()
