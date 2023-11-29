fdc6ece655662f2cc78eb78b6b6757f287229f72
cw <fromnowwon@gmail.com>
2023-11-29 21:11:19 +0900

    07_깊이, 넓이 우선탐색 활용_04_동전 바꿔주기

--- 07_깊이, 넓이 우선탐색 활용/_04_동전 바꿔주기.py
+++ 07_깊이, 넓이 우선탐색 활용/_04_동전 바꿔주기.py
@@ -1,6 +1,15 @@
 import sys
 sys.stdin = open("input.txt", "rt")
 
+def DFS(L, sum):
+    global cnt
+    if L == k:
+        if sum == T:
+            cnt += 1
+    else:
+        for i in range(cn[L] + 1):
+            DFS(L + 1, sum + (cv[L] * i))
+
 if __name__ == "__main__":
     T = int(input())
     k = int(input())
@@ -9,5 +18,8 @@ if __name__ == "__main__":
 
     for i in range(k):
         a, b = map(int, input().split())
-        cv.append()
-        cn.append()
+        cv.append(a)
+        cn.append(b)
+    cnt = 0
+    DFS(0, 0)
+    print(cnt)
