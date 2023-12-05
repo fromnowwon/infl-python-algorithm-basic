ad8e55a2ce46600bb49230bfa23ec67950ea8f79
cw <fromnowwon@gmail.com>
2023-12-05 20:34:37 +0900

    07_깊이, 넓이 우선탐색 활용_05_동전 분배하기(DFS)

--- 07_깊이, 넓이 우선탐색 활용/_05_동전 분배하기(DFS).py
+++ 07_깊이, 넓이 우선탐색 활용/_05_동전 분배하기(DFS).py
@@ -6,6 +6,12 @@ def DFS(L):
     if L == n:
         cha = max(money) - min(money)
 
+        if cha < res:
+            tmp = set()
+            for x in money:
+                tmp.add(x)
+            if len(tmp) == 3:
+                res = cha
     else:
         for i in range(3):
             money[i] += coin[L]
@@ -21,5 +27,5 @@ if __name__ == "__main__":
 
     for _ in range(n):
         coin.append(int(input()))
-
     DFS(0)
+    print(res)
