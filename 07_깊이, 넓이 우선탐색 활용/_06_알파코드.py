cdf44a9e542d44b23dcf0e9b447b122c76a5b115
cw <fromnowwon@gmail.com>
2023-12-09 18:57:13 +0900

    07_깊이, 넓이 우선탐색 활용_06_알파코드

--- 07_깊이, 넓이 우선탐색 활용/_06_알파코드.py
+++ 07_깊이, 넓이 우선탐색 활용/_06_알파코드.py
@@ -10,7 +10,10 @@ def DFS(L, P):
         for i in range(1, 27):
             if code[L] == i:
                 res[P] = i
-
+                DFS(L + 1, P + 1)
+            elif i >= 10 and code[L] == i // 10 and code[L + 1] == i % 10:
+                res[P] = i
+                DFS(L + 2, P + 1)
 
 if __name__ == "__main__":
     code = list(map(int, input()))
