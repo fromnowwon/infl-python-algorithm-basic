7cead88e7a0cfa51aca2a1f9aa60a9aae9ecee71
cw <fromnowwon@gmail.com>
2023-12-16 19:56:14 +0900

    07_깊이, 넓이 우선탐색 활용_08_사과나무

---
+++ 07_깊이, 넓이 우선탐색 활용/_08_사과나무.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open("input.txt", "rt")
+from collections import deque
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+ch = [[0] * n for _ in range(n)]
+sum = 0
+Q = deque()
