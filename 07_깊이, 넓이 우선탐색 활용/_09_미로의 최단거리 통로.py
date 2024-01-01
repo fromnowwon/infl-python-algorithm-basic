e4a0a0e7cf98e47ba6f97b38b3d9daa0bdb9a7c7
cw <fromnowwon@gmail.com>
2024-01-01 23:23:55 +0900

    07_깊이, 넓이 우선탐색 활용_09_미로의 최단거리 통로

--- 07_깊이, 넓이 우선탐색 활용/_09_미로의 최단거리 통로.py
+++ 07_깊이, 넓이 우선탐색 활용/_09_미로의 최단거리 통로.py
@@ -6,3 +6,8 @@ dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 board = [list(map(int, input().split())) for _ in range(7)]
+dis = [[0] * 7 for _ in range(7)]
+
+Q = deque()
+Q.append((0, 0))
+board[0][0] = 1
