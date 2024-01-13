bbc602939b2d832d79fee157752f6943c3f90344
cw <fromnowwon@gmail.com>
2024-01-13 20:18:36 +0900

    07_깊이, 넓이 우선탐색 활용_10_미로 탐색

--- 07_깊이, 넓이 우선탐색 활용/_10_미로 탐색.py
+++ 07_깊이, 넓이 우선탐색 활용/_10_미로 탐색.py
@@ -2,3 +2,9 @@ import sys
 sys.stdin = open("input.txt", "rt")
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
+
+def DFS(x, y):
+    global cnt
+
+if __name__ == "__main__":
+    board = [list(map(int, input().split())) for _ in range(7)]
