2236f6bc37fe63169257512c09e5b43ca3348cd0
cw <fromnowwon@gmail.com>
2024-03-11 16:02:01 +0900

    07_깊이, 넓이 우선탐색 활용_13_섬나라 아일랜드

--- 07_깊이, 넓이 우선탐색 활용/_13_섬나라 아일랜드.py
+++ 07_깊이, 넓이 우선탐색 활용/_13_섬나라 아일랜드.py
@@ -4,3 +4,6 @@ sys.stdin = open("input.txt", "r")
 
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]
 dy = [0, 1, 1, 1, 0, -1, -1, -1]
+
+n = int(input())
+board = [list(map(int(input().split()))) for _ in range(n)]
